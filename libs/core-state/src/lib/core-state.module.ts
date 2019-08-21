763eb6dc3b18bd8ea5285bfbcf6c92f1f289e24e
eccentricnode <shival.bigman@gmail.com>
2019-08-21 15:14:52 -0700

    finishing touches

--- libs/core-state/src/lib/core-state.module.ts
+++ libs/core-state/src/lib/core-state.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { reducers } from '.';
 import { PlayersEffects } from './players/players.effects';
@@ -12,10 +12,10 @@ import { PlayersFacade } from './players/players.facade';
   imports: [
     CommonModule,
     StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([PlayersEffects]),
     StoreDevtoolsModule.instrument({
       maxAge: 10
     }),
-    EffectsModule.forFeature([PlayersEffects]),
   ],
   providers: [PlayersFacade]
 })
